3f495b2e01cfdc934653a2b4717297b2b4ae3a1a
JackyCheung <2541012495@qq.com>
2021-07-29 23:20:56 +0800

    增加视频种类映射表

--- src/controller/video/config.js
+++ src/controller/video/config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    'China': '233.json', //中超
     'England': '239.json', //英超
-    'Spanish': '234.json' //西甲
+    'Spanish': '234.json', //西甲
+    'Italy': '235.json', //意甲
+    'Germany': '236.json', //德甲
+    'More': '237.json', //更多
+    'Leisure': '238.json', //闲情
 }
